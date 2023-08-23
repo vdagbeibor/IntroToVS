3ad530758ba22b4b3bdf4caf937ff5e96d3a3795
vdagbeibor <victoria_agbeibor1@baylor.edu>
2023-08-23 15:37:32 -0500

    initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+a = 1
+b = 2
+c = a + b
+
+for x in range(10):
+    y = x * c
+
+
+message = "Hello World!"
+
+print(message)
+# add in breakpoints 
+
+print()
+# had issues doing cmd + opt + N
+# settings to make code bigger
